6bdf38d45efb5788caddded0001e42695a40dd55
Ángel López <angel.lopez@iesnervion.es>
2022-02-11 11:59:07 +0100

    Cambios en clsCardsManagement en  DAL y BL

--- Kagic/Kagic_DAL/Database/DDL.cs
+++ Kagic/Kagic_DAL/Database/DDL.cs
@@ -37,8 +37,8 @@ namespace Kagic_DAL.Database
         public static void fillDatabase(SQLiteConnection myconnection)
         {
 
-            SQLiteCommand countCreatures = new SQLiteCommand("SELECT COUNT(*) FROM CreatureCard", myconnection);
-            SQLiteCommand countSpells = new SQLiteCommand("SELECT COUNT(*) FROM SpellCard", myconnection);
+            SQLiteCommand countCreatures = new SQLiteCommand("SELECT COUNT(*) FROM CreatureCards", myconnection);
+            SQLiteCommand countSpells = new SQLiteCommand("SELECT COUNT(*) FROM SpellCards", myconnection);
 
             if (Convert.ToInt32(countCreatures.ExecuteScalar()) <= 0)
             {
@@ -53,7 +53,7 @@ namespace Kagic_DAL.Database
 
         private static void fillTableCreatures(SQLiteConnection myconnection)
         {
-            SQLiteCommand createCreatureCard = new SQLiteCommand("INSERT INTO CreatureCard (Name, Description, Image, ManaCost, Life, Attack) " +
+            SQLiteCommand createCreatureCard = new SQLiteCommand("INSERT INTO CreatureCards (Name, Description, Image, ManaCost, Life, Attack) " +
                                                                  "VALUES (\'Gatete Solar\', \'Dispara fuego por las orejas\', \'\\Assets\\PRUEBAS\\solar_kitten.jpg\', 3, 3, 3 )", myconnection);
 
             createCreatureCard.ExecuteNonQuery();
@@ -61,14 +61,14 @@ namespace Kagic_DAL.Database
 
         private static void fillTableSpells(SQLiteConnection myconnection)
         {
-            SQLiteCommand createSpellCard = new SQLiteCommand("INSERT INTO SpellCard (Name, Description, Image, ManaCost, Effect, IsDamage, IsArea) " +
+            SQLiteCommand createSpellCard = new SQLiteCommand("INSERT INTO SpellCards (Name, Description, Image, ManaCost, Effect, IsDamage, IsArea) " +
                                                               "VALUES (\'Seta Venenosa\',\'Envenena a la criatura objetivo\', \'\\Assets\\PRUEBAS\\CartaSeta.png\', 4, 4, 1, 0)", myconnection);
             createSpellCard.ExecuteNonQuery();
         }
 
         private static void createTables(SQLiteConnection myconnection)
         {
-            SQLiteCommand createCreatureCard = new SQLiteCommand("CREATE TABLE IF NOT EXISTS CreatureCard(" +
+            SQLiteCommand createCreatureCard = new SQLiteCommand("CREATE TABLE IF NOT EXISTS CreatureCards(" +
                                                                     "[Id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
                                                                     "[Name] VARCHAR(50) NOT NULL," +
                                                                     "[Description] VARCHAR(255) NULL," +
@@ -77,7 +77,7 @@ namespace Kagic_DAL.Database
                                                                     "[Life] INTEGER NOT NULL," +
                                                                     "[Attack] INTEGER NOT NULL)", myconnection);
 
-            SQLiteCommand createSpellCard = new SQLiteCommand("CREATE TABLE IF NOT EXISTS SpellCard(" +
+            SQLiteCommand createSpellCard = new SQLiteCommand("CREATE TABLE IF NOT EXISTS SpellCards(" +
                                                                 "[Id] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
                                                                 "[Name] VARCHAR(50) NOT NULL," +
                                                                 "[Description] VARCHAR(255) NULL," +
@@ -90,69 +90,5 @@ namespace Kagic_DAL.Database
             createCreatureCard.ExecuteNonQuery();
             createSpellCard.ExecuteNonQuery();
         }
-
-        //public static void createDatabase()
-        //{
-
-        //    SQLiteConnection sqlite_conn;
-        //    // Create a new database connection:
-        //    sqlite_conn = new SQLiteConnection("Data Source = kagic.db; Version = 3; New = True; Compress = True; ");
-        // // Open the connection:
-        // try
-        //    {
-        //        sqlite_conn.Open();
-        //        sqlite_conn.Close();
-        //    }
-        //    catch
-        //    {
-        //        throw;
-        //    }
-
-        //}
-
-        //static void CreateTable(SQLiteConnection conn)
-        //{
-
-        //    SQLiteCommand sqlite_cmd;
-        //    string Createsql = "CREATE TABLE SampleTable (Col1 VARCHAR(20), Col2 INT)";
-        //   string Createsql1 = "CREATE TABLE SampleTable1 (Col1 VARCHAR(20), Col2 INT)";
-        //   sqlite_cmd = conn.CreateCommand();
-        //    sqlite_cmd.CommandText = Createsql;
-        //    sqlite_cmd.ExecuteNonQuery();
-        //    sqlite_cmd.CommandText = Createsql1;
-        //    sqlite_cmd.ExecuteNonQuery();
-
-        //}
-
-        //static void InsertData(SQLiteConnection conn)
-        //{
-        //    SQLiteCommand sqlite_cmd;
-        //    sqlite_cmd = conn.CreateCommand();
-        //    sqlite_cmd.CommandText = "INSERT INTO SampleTable (Col1, Col2) VALUES('Test Text ', 1); ";
-        //   sqlite_cmd.ExecuteNonQuery();
-        //    sqlite_cmd.CommandText = "INSERT INTO SampleTable (Col1, Col2) VALUES('Test1 Text1 ', 2); ";
-        //   sqlite_cmd.ExecuteNonQuery();
-        //    sqlite_cmd.CommandText = "INSERT INTO SampleTable (Col1, Col2) VALUES('Test2 Text2 ', 3); ";
-        //   sqlite_cmd.ExecuteNonQuery();
-        //    sqlite_cmd.CommandText = "INSERT INTO SampleTable1 (Col1, Col2) VALUES('Test3 Text3 ', 3); ";
-        //   sqlite_cmd.ExecuteNonQuery();
-
-        //}
-
-        //static void ReadData(SQLiteConnection conn)
-        //{
-        //    SQLiteDataReader sqlite_datareader;
-        //    SQLiteCommand sqlite_cmd;
-        //    sqlite_cmd = conn.CreateCommand();
-        //    sqlite_cmd.CommandText = "SELECT * FROM SampleTable";
-
-        //    sqlite_datareader = sqlite_cmd.ExecuteReader();
-        //    while (sqlite_datareader.Read())
-        //    {
-        //        string myreader = sqlite_datareader.GetString(0);
-        //        Console.WriteLine(myreader);
-        //    }
-        //    conn.Close();
-        //}
     }
 }
