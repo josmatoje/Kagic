01bdc0033b90e08cbb7c124f9b9c826c77b85399
Ángel López <angel.lopez@iesnervion.es>
2022-03-07 09:04:48 +0100

    he valorado una cosa y ya no deberia entrar en bucle

--- Kagic/Kagic_UI/ViewModels/VMFight.cs
+++ Kagic/Kagic_UI/ViewModels/VMFight.cs
@@ -489,6 +489,10 @@ namespace Kagic_UI.ViewModels
                         Creaturebattle(enemyCreatureIndex, iaCreatureIndex);
                         await Task.Delay(1000);
                     }
+                    else
+                    {
+                        isPlayerTurn = false; //A binding error has ocurred and it is necesary to redefine isPlayerTurn 
+                    }
                 }
                 else
                 {
