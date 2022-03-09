94ab79a2c408905bb676a5e206b2a82707b720e7
josmatoje <josemariamataojeda@gmail.com>
2022-03-09 13:38:21 +0100

    empieza a generar lista

---
+++ Kagic/Kagic_UI/ViewModels/UtilitiesVM/clsCardList.cs
@@ -0,0 +1,25 @@
+﻿using Kagic_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kagic_UI.ViewModels
+{
+    public class clsCardList
+    {
+        List<clsCard> cardList = insertCard();
+
+        public List<clsCard> CardList { get => cardList; }
+
+        private static List<clsCard> insertCard()
+        {
+            List<clsCard> cardList = new List<clsCard>();
+
+
+
+            return cardList;
+        }
+    }
+}
